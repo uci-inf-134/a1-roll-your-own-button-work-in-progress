3ed5a681518c9c1f9368aac8984a0deb1185d152
Ashley Ly <skywings173@gmail.com>
2024-04-16 15:28:45 -0700

    font added and size adjusted

--- src/widgets/button.ts
+++ src/widgets/button.ts
@@ -1,26 +1,27 @@
 // importing local code, code we have written
-import {IdleUpWidgetState, PressedWidgetState } from "../core/ui";
-import {Window, Widget, RoleType, EventArgs} from "../core/ui";
+import { IdleUpWidgetState, PressedWidgetState } from "../core/ui";
+import { Window, Widget, RoleType, EventArgs } from "../core/ui";
 // importing code from SVG.js library
-import {Rect, Text, Box} from "../core/ui";
+import { Rect, Text, Box } from "../core/ui";
 
-class Button extends Widget{
+class Button extends Widget {
     private _rect: Rect;
+    private _eventrect: Rect;
     private _text: Text;
     private _input: string;
     private _fontSize: number;
-    private _text_y: number;
     private _text_x: number;
-    private defaultText: string= "Button";
-    private defaultFontSize: number = 18;
-    private defaultWidth: number = 80;
+    private _text_y: number;
+    private defaultText: string = "Button";
+    private defaultFontSize: number = 20;
+    private defaultWidth: number = 100;
     private defaultHeight: number = 30;
 
-    private fontFamily: string = 'Helvetica';
-    private defaultRadius: number = 10;
-  
+    private fontFamily:string = 'Helvetica';
 
-    constructor(parent:Window){
+
+
+    constructor(parent: Window) {
         super(parent);
         // set defaults
         this.height = this.defaultHeight;
@@ -37,11 +38,6 @@ class Button extends Widget{
         this.selectable = false;
     }
 
-    set fontSize(size:number){
-        this._fontSize= size;
-        this.update();
-    }
-
     get label(): string {
         return this._input;
     }
@@ -51,91 +47,87 @@ class Button extends Widget{
         this.update();
     }
 
-    
-    // custom size set
-    set widthSize(size: number)
-    {
-        this._rect.attr('width',size);
+    set fontSize(size: number) {
+        this._fontSize = size;
         this.update();
     }
 
-    set heightSize(size:number)
-    {
+    // custom size set
+    set widthSize(size: number) {
+        this._rect.attr('width', size);
+        this.update();
+    }
+
+    set heightSize(size: number) {
         this._rect.attr('height', size);
         this.update();
     }
 
     //get custom size
-    get widthSize()
-    {
+    get widthSize() {
         return this.width;
     }
-    get heightSize(){
+
+    get heightSize() {
         return this.height;
     }
 
-
-    private positionText(){
-        let box:Box = this._text.bbox();
+    private positionText() {
+        let box: Box = this._text.bbox();
         // in TS, the prepending with + performs a type conversion from string to number
-        this._text_y = (+this._rect.y() + ((+this._rect.height()/2)) - (box.height/2));
+        this._text_y = (+this._rect.y() + ((+this._rect.height() / 2)) - (box.height / 2));
         this._text.x(+this._rect.x() + 4);
-        if (this._text_y > 0){
+        if (this._text_y > 0) {
             this._text.y(this._text_y);
         }
     }
-    
+
     render(): void {
         this._group = (this.parent as Window).window.group();
-        this._rect = this._group.rect(this.width, this.height);
-       
-        //custom color w/ rounded edges
-        this._rect.fill('#ADD8E6');
-        this._rect.radius(this.defaultRadius);
-       
-        //adjusting text
+        this._rect = this._group.rect(this.width, this.height).radius(10); // Set radius here for rounded corners
+        this._rect.fill("#ADD8E6");
+        
         this._text = this._group.text(this._input);
-        this._text.attr('text-length', this.width);
-        this._text.attr('text-align', 'center');
-        this._text.font('family', this.fontFamily);
-
         // Set the outer svg element 
         this.outerSvg = this._group;
-        // Add a transparent rect on top of text to 
-        // prevent selection cursor and to handle mouse events
-        let eventrect = this._group.rect(this.width, this.height).opacity(0).attr('id', 0);
-
+        // Add a transparent rect on top of text to prevent selection cursor and to handle mouse events
+        this._eventrect = this._group.rect(this.width, this.height).opacity(0);
         // register objects that should receive event notifications.
-        // for this widget, we want to know when the group or rect objects
-        // receive events
-        this.registerEvent(eventrect);
+        this.registerEvent(this._eventrect);
     }
 
     override update(): void {
-        if(this._text != null)
-            this._text.font('size', this._fontSize);
+        if (this._text) {
+            this._text.font({ size: this._fontSize });
             this._text.text(this._input);
-            
-            this.positionText();
+            this._text.font('family', this.fontFamily);
+            let box: Box = this._text.bbox();
+            this.width = box.width + 10;
+            this._rect.size(this.width, this.height).radius(10); // Ensure the radius is maintained on update
+            this._eventrect.size(this.width, this.height);
+        }
 
-        if(this._rect != null)
+        this.positionText();
+
+        if (this._rect != null) {
             this._rect.fill(this.backcolor);
+        }
         
         super.update();
     }
-    
-    pressReleaseState(): void{
 
+    pressReleaseState(): void {
         if (this.previousState instanceof PressedWidgetState)
             this.raise(new EventArgs(this));
     }
 
-   
+    //TODO: implement the onClick event using a callback passed as a parameter
     onClick(callback: { (event?: any): void }): void {
         this.attach(callback);
     }
 
-    
+    //TODO: give the states something to do! Use these methods to control the visual appearance of your
+    //widget
     idleupState(): void {
         this._rect.fill('#ADD8E6');
     }
@@ -155,11 +147,15 @@ class Button extends Widget{
         this._rect.fill('#1E90FF');
     }
     moveState(): void {
-        console.log("moveState");
+        this._rect.fill('#B0C4DE');
     }
     keyupState(keyEvent?: KeyboardEvent): void {
-        console.log("keyupState");
+        if (keyEvent && keyEvent.key === 'Enter') {
+            this.onClick(() => {
+                console.log("Button clicked via keyboard");
+            });
+        }
     }
 }
 
-export {Button}
+export { Button }
